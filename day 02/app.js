4b9bc31c608c5828e9fe5e4ca34e254ad7caea47
MUHAMMADFARHANHANIF <farhansmit0318@gamil.com>
2024-07-14 03:54:30 -0700

    day 02 cmpleted

---
+++ day 02/app.js
@@ -0,0 +1,113 @@
+//activty 01
+
+let a = 20
+let b = 20 
+// 01
+console.log(a+b);// 40
+
+// 02
+console.log(a-b);// 0
+
+// 03 
+console.log(a*b);// 400
+
+// 04
+console.log(a/b);// 1
+
+// 05
+console.log(a%b);// 0    //reminder is zero
+
+
+
+// activity 02
+
+// 01
+a += 10
+console.log(a); // 30
+
+// 02
+b -= 10 
+console.log(b);// 10
+
+
+
+
+
+// activity 03 
+
+// 01
+
+// a = 30
+// b = 10
+console.log(a>b); //true
+console.log(a<b);// false
+
+// 02
+
+// a = 30
+// b = 10
+console.log(a>=b); // true      a is greter and equal to b
+console.log(a<=b);// false      a is lessthen and equal to b   
+
+
+// 03
+
+// a = 30
+// b = 10
+console.log(a==b);//false      only value check
+console.log(a===b);//false     value and type both check
+
+
+
+
+
+// activity 04
+
+// 01
+
+// a=30
+// b=10
+console.log(a==30 && b==20);  // false
+
+
+// 02
+// a=30
+// b=10
+
+console.log(a==30 || b==20); //true
+
+
+// 03
+
+// a=30
+// b=10
+console.log(a!=30);// false
+console.log(a!=10);// true
+console.log(b!=10);//false
+console.log(b!=30);//true
+
+
+
+
+
+// activity 05 
+
+number = 100 
+number >= 0  ? console.log("number is positive"):console.log("number is nagative"); //  number is positive
+
+number = -100
+number >= 0  ? console.log("number is positive"):console.log("number is nagative"); // number is nagative
+
+
+
+
+
+
+// feature request 
+
+// 01
+        
+console.log((10 + 20)+(10-20)-(10*20)*(10/20)/(10%20));// 20
+
+
+
